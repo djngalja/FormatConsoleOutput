2cce7560d745cf707d06edd86f560447689c6ab4
Galina <djngalja@gmail.com>
2023-08-11 22:26:20 +0300

    Updated table(vararg): user can add a header. Updated makeBorder(): user can use any chars to create a border.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,21 +1,42 @@
 import kotlin.math.ceil
 
 fun main() {
-    val text = "Making my way downtown"
     println("I. Border around String:")
+    val text = "Making my way downtown"
     text.frame()
 
-    val list = listOf("eggs", "bread", "oranges", "tea", "watermelon")
     println("II. Border around List<String>, align = right:")
+    val list = listOf("eggs", "bread", "oranges", "tea", "watermelon")
     list.frame("right")
+
     println("III. Single column table with header, align = middle:")
     list.table("Groceries", "middle")
 
-    val list1 = mutableListOf("eggs", "bread", "oranges", "tea", "watermelon")
-    val list2 = mutableListOf("trousers", "shirt", "socks", "tie")
-    val list3 = mutableListOf("cream", "mascara", "toner")
-    println("IV. Table with 3 columns, align = left:")
+    println("IV. Table with 3 columns:")
+    val list1 = mutableListOf("fruits", "oranges", "apples", "strawberries", "grapes")
+    val list2 = mutableListOf("vegetables", "tomatoes", "potatoes", "onions")
+    val list3 = mutableListOf("dairy", "butter", "cheese")
     table(list1, list2, list3)
+
+    println("V. Table with 4 columns and header:")
+    val list4 = mutableListOf("snacks", "popcorn", "nuts")
+    table(list1, list2, list3, list4, header = true)
+
+    println("VI. Table with 5 columns, header, total, align = middle:")
+    val l0 = mutableListOf(1,2,3,4,5,6,7,8)
+    val l0new = mutableListOf<String>()
+    for (num in l0) l0new.add(num.toString())
+    val l1 = mutableListOf("Product", "Chocolate", "Gummibarchen", "Scottish Longbreads", "Sir Rodney's Scones", "Tarte au sucre", "Chocolate Biscuits", "Total")
+    val l2 = mutableListOf(744.60, 5079.60, 1267.50, 1418.00, 4728.00, 943.89, 14181.59)
+    val l2new = mutableListOf("Qtr 1")
+    for (num in l2) l2new.add(num.toString())
+    val l3 = mutableListOf(162.56, 1249.20, 1062.50, 756.00, 4547.92, 349.60, 8127.78)
+    val l3new = mutableListOf("Qtr 2")
+    for (num in l3) l3new.add(num.toString())
+    val l4 = mutableListOf(907.16, 6328.80, 2330.00, 2174.00, 9275.92, 1293.49, 22309.37)
+    val l4new = mutableListOf("Grand Total")
+    for (num in l4) l4new.add(num.toString())
+    table(l0new, l1, l2new, l3new, l4new, header = true, total = true, align = "middle")
 }
 
 /*
@@ -73,11 +94,15 @@ fun List<String>.table(header: String, align: String = "left") {
 
 /*
 * Joins any number of lists into a table and prints out the results.
+* If `header` is set to `true`, the 1st string of every list becomes a header.
 * By default, the text in the table is aligned left.
 * To center the text, set `align` to `middle`.
 * To align the text right, set `align` to `right`.
 */
-fun table(vararg columns: MutableList<String>, align: String = "left") {
+fun table( vararg columns: MutableList<String>,
+           header: Boolean = false,
+           total: Boolean = false,
+           align: String = "left") {
     var numOfRows = 0
     val longestWords = mutableListOf<String>()
     for (column in columns) {
@@ -92,7 +117,8 @@ fun table(vararg columns: MutableList<String>, align: String = "left") {
         border += makeBorder(longestWord)
         border = border.dropLast(1)
     }
-    println("$border+")
+    border  += '+'
+    println(border)
     for (i in 0..<numOfRows){
         print("| ")
         for (j in columns.indices) {
@@ -100,17 +126,22 @@ fun table(vararg columns: MutableList<String>, align: String = "left") {
             print(" | ")
         }
         println()
+        if (header && i == 0) println(border)
+        if (total && (i == numOfRows - 2)) {
+            val ttlBorder = border.replace('=', '-')
+            println(ttlBorder)
+        }
     }
-    println("$border+")
+    println(border)
 }
 
 /*
 * Creates and returns a horizontal border for tables and frames.
 */
-fun makeBorder(longestWord: String): String {
-    var border = "+-"
-    for (i in 1..longestWord.length) border += '-'
-    border += "-+"
+fun makeBorder(longestWord: String, char: Char = '='): String {
+    var border = "+$char"
+    for (i in 1..longestWord.length) border += char
+    border += "$char+"
     return border
 }
 
